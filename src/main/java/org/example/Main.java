2491f4a2d078a13fce01e93e55f2de0eac741b0f
Lourenzo Ferreira <lourenzo@gmail.com>
2022-11-22 07:21:37 -0300

    chore: initialize module using Intellij's new project and the sample code from the exercise

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String ... args) {
+        Map<Integer, Integer> registerStatus = new HashMap<Integer, Integer>();
+        registerStatus.put(1, 30); // One Dolar, 30 bills;
+        registerStatus.put(2, 3); // Two Dolar, 3 bills;
+        registerStatus.put(5, 3); // Five Dolar, 3 bills;
+        registerStatus.put(10, 2); // Ten Dolar, 2 bills;
+        registerStatus.put(20, 1); // twenty Dolar, 2 bills;
+        registerStatus.put(50, 0); // fifhty Dolar, 0 bills;
+        registerStatus.put(100, 0); // One hundred Dolar, 0 bills;
+
+        boolean result = canIGiveChange(registerStatus, 100, 9);
+        System.out.printf("Result " + result);
+
+    }
+
+    public static boolean canIGiveChange(Map<Integer,Integer> registerStatus, Integer customerPayment, Integer productCost) {
+
+        return false;
+    }
+}
